f00f50ece13ef3a89d74f95f33ce2c20597e93f1
walson <wxfcdd3698@163.com>
2017-04-12 14:00:23 +0800

    Rejected commit: Default

---
+++ utils.java
@@ -0,0 +1,14 @@
+package com.iflytek.walson.iflytektestdemo;
+
+import android.content.Context;
+import android.widget.Toast;
+
+/**
+ * Created by Administrator on 2017/4/12.
+ */
+
+public class utils {
+    public static void showToast(Context context, String content) {
+        Toast.makeText(context, content, Toast.LENGTH_SHORT).show();
+    }
+}
